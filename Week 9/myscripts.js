25d739e72f26b8e114585fa1c4c8c4cec1b77508
fazad101 <frasheed101@gmail.com>
2022-12-06 23:40:08 -0500

    Before animations

--- Week 9/myscripts.js
+++ Week 9/myscripts.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector("nav");
 const burger = document.querySelector(".burger");
-const workDivOne = document.querySelector("#work div:nth-child(1)");
-const workDivTwo = document.querySelector("#work div:nth-child(2)");
-const workDivThree = document.querySelector("#work div:nth-child(3)");
-const workDivFour = document.querySelector("#work div:nth-child(4)");
-const work = document.querySelector("#work");
+
+const intro = document.querySelector(".intro");
+// const workDivOne = document.querySelector("#work div:nth-child(1)");
+// const workDivTwo = document.querySelector("#work div:nth-child(2)");
+// const workDivThree = document.querySelector("#work div:nth-child(3)");
+// const workDivFour = document.querySelector("#work div:nth-child(4)");
+// const work = document.querySelector("#work");
 
 const mainContainer = document.querySelector(".main-container");
 
@@ -13,6 +15,16 @@ burger.addEventListener("click", () => nav.classList.toggle("toggle-nav"));
 burger.addEventListener("click", () =>
   mainContainer.classList.toggle("toggle-container")
 );
+
+//            SCROLL EVENT
+window.onscroll = () => {
+  let top = window.pageYOffset;
+  if (top > 200) {
+    intro.classList.add("hidden");
+  } else {
+    intro.classList.remove("hidden");
+  }
+};
 
 // workDivOne.addEventListener("mouseover", () =>
 //   document
